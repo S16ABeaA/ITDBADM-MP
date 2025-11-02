3ee74fd4e7626c566cec25632ffc688a3b2f0daf
S16AbeaA <adrian_bea@dlsu.edu.ph>
2025-11-02 08:21:42 +0800

    homepage mad

---
+++ homepage.php
@@ -0,0 +1,329 @@
+<?php include("header.html");?>
+<!DOCTYPE html>
+<html>
+<head>
+  <script src="https://kit.fontawesome.com/a39233b32c.js" crossorigin="anonymous"></script>
+  <link href="https://fonts.googleapis.com/css2?family=Bungee&family=Lato&display=swap" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Bungee+Shade&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="./css/main.css?v=1.1">
+  <link rel="stylesheet" href="./css/homepage.css?v=1.1">
+  <title>Homepage</title>
+</head>
+<body>
+  <div class="content-section">
+    <div class="welcome">
+        Welcome To AnimoBowl
+      </div>
+     <button class="get-started-button" 
+      onclick="location.href = 'login-signup.php'">Sign up now</button>
+
+    <div class="bowling-ball-section">
+      <h2>Bowling Ball</h2>
+      <div class="hp-product-row">
+        <div class="hp-product-container">
+          <img class="hp-product-image" src="./images/bowlingball1.png">
+          <h5 class="hp-product-name">
+            Bowling Ball
+          </h5>
+          <h2 class="hp-product-price">
+            P1159.99
+          </h2>
+        </div>
+
+         <div class="hp-product-container">
+          <img class="hp-product-image" src="./images/bowlingball1.png">
+          <h5 class="hp-product-name">
+            Bowling Ball
+          </h5>
+          <h2 class="hp-product-price">
+            P1159.99
+          </h2>
+        </div>
+
+         <div class="hp-product-container">
+          <img class="hp-product-image" src="./images/bowlingball1.png">
+          <h5 class="hp-product-name">
+            Bowling Ball
+          </h5>
+          <h2 class="hp-product-price">
+            P1159.99
+          </h2>
+        </div>
+
+         <div class="hp-product-container">
+          <img class="hp-product-image" src="./images/bowlingball1.png">
+          <h5 class="hp-product-name">
+            Bowling Ball
+          </h5>
+          <h2 class="hp-product-price">
+            P1159.99
+          </h2>
+        </div>
+
+         <div class="hp-product-container">
+          <img class="hp-product-image" src="./images/bowlingball1.png">
+          <h5 class="hp-product-name">
+            Bowling Ball hwrb hiewbqjebrifvjerbfiherbr
+          </h5>
+          <h2 class="hp-product-price">
+            P1159.99
+          </h2>
+        </div>
+      </div>
+      <div class="display-view-btn">
+        <button class="view-more-btn">
+          View More
+        </button>
+      </div>
+    </div>
+
+    <div class="bowling-shoes-section">
+      <h2>Bowling Shoes</h2>
+      <div class="hp-product-row">
+        <div class="hp-product-container">
+          <img class="hp-product-image" src="./images/bowlingshoes1.png">
+          <h5 class="hp-product-name">
+            Bowling Shoes
+          </h5>
+          <h2 class="hp-product-price">
+            P1159.99
+          </h2>
+        </div>
+
+         <div class="hp-product-container">
+          <img class="hp-product-image" src="./images/bowlingshoes1.png">
+          <h5 class="hp-product-name">
+            Bowling Shoes
+          </h5>
+          <h2 class="hp-product-price">
+            P1159.99
+          </h2>
+        </div>
+
+         <div class="hp-product-container">
+          <img class="hp-product-image" src="./images/bowlingshoes1.png">
+          <h5 class="hp-product-name">
+            Bowling Shoes
+          </h5>
+          <h2 class="hp-product-price">
+            P1159.99
+          </h2>
+        </div>
+
+         <div class="hp-product-container">
+          <img class="hp-product-image" src="./images/bowlingshoes1.png">
+          <h5 class="hp-product-name">
+            Bowling Shoes
+          </h5>
+          <h2 class="hp-product-price">
+            P1159.99
+          </h2>
+        </div>
+
+         <div class="hp-product-container">
+          <img class="hp-product-image" src="./images/bowlingshoes1.png">
+          <h5 class="hp-product-name">
+            Bowling Shoes
+          </h5>
+          <h2 class="hp-product-price">
+            P1159.99
+          </h2>
+        </div>
+      </div>
+      <div class="display-view-btn">
+        <button class="view-more-btn">
+          View More
+        </button>
+      </div>
+    </div>
+
+    <div class="bowling-bag-section">
+      <h2>Bowling Bag</h2>
+      <div class="hp-product-row">
+        <div class="hp-product-container">
+          <img class="hp-product-image" src="./images/bowlingbag1.png">
+          <h5 class="hp-product-name">
+            Bowling Bag
+          </h5>
+          <h2 class="hp-product-price">
+            P1159.99
+          </h2>
+        </div>
+
+         <div class="hp-product-container">
+          <img class="hp-product-image" src="./images/bowlingbag1.png">
+          <h5 class="hp-product-name">
+            Bowling Bag
+          </h5>
+          <h2 class="hp-product-price">
+            P1159.99
+          </h2>
+        </div>
+
+         <div class="hp-product-container">
+          <img class="hp-product-image" src="./images/bowlingbag1.png">
+          <h5 class="hp-product-name">
+            Bowling Bag
+          </h5>
+          <h2 class="hp-product-price">
+            P1159.99
+          </h2>
+        </div>
+
+         <div class="hp-product-container">
+          <img class="hp-product-image" src="./images/bowlingbag1.png">
+          <h5 class="hp-product-name">
+            Bowling Bag
+          </h5>
+          <h2 class="hp-product-price">
+            P1159.99
+          </h2>
+        </div>
+
+         <div class="hp-product-container">
+          <img class="hp-product-image" src="./images/bowlingbag1.png">
+          <h5 class="hp-product-name">
+            Bowling Bag
+          </h5>
+          <h2 class="hp-product-price">
+            P1159.99
+          </h2>
+        </div>
+      </div>
+      <div class="display-view-btn">
+        <button class="view-more-btn">
+          View More
+        </button>
+      </div>
+    </div>
+
+    <div class="bowling-accessories-section">
+      <h2>Bowling Accessories</h2>
+      <div class="hp-product-row">
+        <div class="hp-product-container">
+          <img class="hp-product-image" src="./images/wristsupport.png">
+          <h5 class="hp-product-name">
+            Wrist Support
+          </h5>
+          <h2 class="hp-product-price">
+            P1159.99
+          </h2>
+        </div>
+
+         <div class="hp-product-container">
+          <img class="hp-product-image" src="./images/wristsupport.png">
+          <h5 class="hp-product-name">
+            Wrist Support
+          </h5>
+          <h2 class="hp-product-price">
+            P1159.99
+          </h2>
+        </div>
+
+         <div class="hp-product-container">
+          <img class="hp-product-image" src="./images/wristsupport.png">
+          <h5 class="hp-product-name">
+            Wrist Support
+          </h5>
+          <h2 class="hp-product-price">
+            P1159.99
+          </h2>
+        </div>
+
+         <div class="hp-product-container">
+          <img class="hp-product-image" src="./images/gloves.png">
+          <h5 class="hp-product-name">
+            Gloves
+          </h5>
+          <h2 class="hp-product-price">
+            P1159.99
+          </h2>
+        </div>
+
+         <div class="hp-product-container">
+          <img class="hp-product-image" src="./images/gloves.png">
+          <h5 class="hp-product-name">
+            Gloves
+          </h5>
+          <h2 class="hp-product-price">
+            P1159.99
+          </h2>
+        </div>
+      </div>
+      <div class="display-view-btn">
+        <button class="view-more-btn">
+          View More
+        </button>
+      </div>
+    </div>
+
+    <div class="cleaning-supplies-section">
+      <h2>Cleaning Supplies</h2>
+      <div class="hp-product-row">
+        <div class="hp-product-container">
+          <img class="hp-product-image" src="./images/cleaningsupplies.png">
+          <h5 class="hp-product-name">
+            Cleaning Supplies
+          </h5>
+          <h2 class="hp-product-price">
+            P1159.99
+          </h2>
+        </div>
+
+         <div class="hp-product-container">
+          <img class="hp-product-image" src="./images/cleaningsupplies.png">
+          <h5 class="hp-product-name">
+            Cleaning Supplies
+          </h5>
+          <h2 class="hp-product-price">
+            P1159.99
+          </h2>
+        </div>
+
+         <div class="hp-product-container">
+          <img class="hp-product-image" src="./images/cleaningsupplies.png">
+          <h5 class="hp-product-name">
+            Cleaning Supplies
+          </h5>
+          <h2 class="hp-product-price">
+            P1159.99
+          </h2>
+        </div>
+
+         <div class="hp-product-container">
+          <img class="hp-product-image" src="./images/cleaningsupplies.png">
+          <h5 class="hp-product-name">
+            Cleaning Supplies
+          </h5>
+          <h2 class="hp-product-price">
+            P1159.99
+          </h2>
+        </div>
+
+         <div class="hp-product-container">
+          <img class="hp-product-image" src="./images/cleaningsupplies.png">
+          <h5 class="hp-product-name">
+            Cleaning Supplies
+          </h5>
+          <h2 class="hp-product-price">
+            P1159.99
+          </h2>
+        </div>
+      </div>
+      <div class="display-view-btn">
+        <button class="view-more-btn">
+          View More
+        </button>
+      </div>
+    </div>
+
+
+  </div>
+</body>
+
+</html>
+
+<?php include("footer.html");?>
