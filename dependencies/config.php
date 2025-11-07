1537ab69690252b06db8b28fd4559dae8409843d
OatiesMilk <dylanakia2002@gmail.com>
2025-11-08 02:23:47 +0800

    Updated database configuration and connection settings

--- dependencies/config.php
+++ dependencies/config.php
@@ -1,13 +1,13 @@
 <?php
-define('DB_SERVER', 'localhost');   // db server
-define('DB_USERNAME', 'root');      // username
-define('DB_PASSWORD', 'Dlsu1234!'); // password
-define('DB_NAME', 'itdbadm');       // database name
+define('DB_SERVER', '127.0.0.1');     
+define('DB_USERNAME', 'student4');  
+define('DB_PASSWORD', 'Dlsu1234!');   
+define('DB_NAME', 'AnimoBowl');       
+define('DB_PORT', 3306);         
 
 try {
-    $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-    
-    // Check connection
+    $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
+
     if ($conn->connect_error) {
         throw new Exception("Connection failed: " . $conn->connect_error);
     }
